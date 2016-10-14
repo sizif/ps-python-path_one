58c86af64b5c93879c00ae2df0c780ae91bde0eb
ajdika <ajdika@live.com>
2016-10-14 03:25:32 -0400

    Change the named parameter 'end' from '\n' to ''

--- wordgames.py
+++ wordgames.py
@@ -7,7 +7,7 @@ def get_random_word():
 
 def show_word(word):
     for character in word:
-        print(character) # characters printed on separate lines!
+        print(character, end="")
 
 def play_word_game():
     strikes = 0
