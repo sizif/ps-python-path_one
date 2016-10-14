eb908a14cf3ee9aabd8880d390c3965f4ebd79c3
ajdika <ajdika@live.com>
2016-10-14 07:23:33 -0400

    (1) while True: is a potential infinite loop; (2) the way to stop it is if the user presses enter; (3) break out of the loop

--- shopping.py
+++ shopping.py
@@ -2,5 +2,11 @@
 
 def go_shopping():
   cart = []
+  
+  while True: # (1) potential infinite loop
+    item = get_item()
+    if item == "": # (2) if the user just presses enter
+        break # (3) exit the loop
+    cart.append(item)
 
 go_shopping()
