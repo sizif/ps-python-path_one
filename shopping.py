fd32a6bbe268ff1581baba31932071475b82f795
ajdika <ajdika@live.com>
2016-10-14 08:26:03 -0400

    (i) Python Sequences (strings and lists) are used for managing a collection of items.

--- shopping.py
+++ shopping.py
@@ -2,8 +2,8 @@ def get_order():
     print("[command] [item] (command is a to add, d to delete, q to quit)")
     line = input()
     
-    command = line[:1] # (1) line[:1] = start at sub[0], end at [1];
-    item = line[2:] # (2) line[2:] = start at sub[2], end at the end of list;
+    command = line[:1] 
+    item = line[2:] 
     
 def go_shopping():
     cart = []
@@ -19,3 +19,5 @@ def go_shopping():
     
     
 go_shopping()
+
+# (i) Python Sequences (strings and lists) are used for managing a collection of items.
