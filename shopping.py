0eb1d8f16b6301d315788beaab5b718c3c630a09
ajdika <ajdika@live.com>
2016-10-14 10:25:20 -0400

    An example of using a tupple, (i) create it; (2) also with round brackets; (3) Tupple with IN operator; (4) you can offset tupples, just like a list; (5) tuple unpacking

--- shopping.py
+++ shopping.py
@@ -1,10 +1,18 @@
 def get_order():
     print("[command] [item] (command is a to add, d to delete, q to quit)")
     line = input()
-    
+
     command = line[:1] 
     item = line[2:] 
-    
+    order = command, item # (i) To create a tupple (another sequence type to manage multiple items):
+    # (i2) I can also create a tuple with round brackets: order = (command, item)
+    # (i3) Tupple with in operator: "apples" in order 
+    # the above returns True
+    # (i4) Just like a list, I can say, what's offset[0], what's offset[1], like this:
+    # order[0]
+    # order[1]
+    # a tuple is like a list, but it's faster and safer (cuz you can't append or delete from it)
+    # "tuple unpacking": c,i = order
 def go_shopping():
     cart = []
   
@@ -20,4 +28,4 @@ def go_shopping():
     
 go_shopping()
 
-# (i) Python Sequences (strings and lists) are used for managing a collection of items.
+
