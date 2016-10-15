6f375fe6196b71a5dda5305a8a76f9d8821d76a2
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 15:08:05 +0200

    A note on ABSTRACTIONS and ENCAPSULATION

---
+++ oopencapsulation.py
@@ -0,0 +1,4 @@
+# Remember, abstractions generalize and simplify our code by reducing complexity.
+# One way oop reduces complexity is by providing encapsulation.
+# One goal of encapsulation is to hide details. I don't care how append works in a list, all I want to know is that I can use it.
+# Another one is grouping details into a cohesive model, into a class
