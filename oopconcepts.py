a0a571225a638f8040075b08f31f6cefbf27a3c9
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 14:33:47 +0200

    (i) Look at comments at the bottom of the file; a variable can contain a number of the box that you have to look in

--- oopconcepts.py
+++ oopconcepts.py
@@ -12,3 +12,7 @@ p1.say_hello()
 
 p2 = Person("Allen")
 p2.say_hello()
+
+# Objects live in memory
+# variables are pointers, or references, to the location in the memory at which the object 'lives'
+# It's like trying to get a piece of information from only 1 of a million boxes; a variable can contain the number of the box that you need to look in
