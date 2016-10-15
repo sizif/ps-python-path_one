e67f9f82ff2c8d79457eb1f67eeabd16a0bc3d78
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 15:16:15 +0200

    # (1) Combining Person and list, imagine we had to write an application for managing a classroom # (2) So we'll have to handle multiple Person objects and group them into a specific classroom # (3) Now I can start appending Person objects into that list

--- oopconcepts.py
+++ oopconcepts.py
@@ -8,9 +8,10 @@ class Person:
         print(id(self)) # (3)
         print("Hello, ", self.name)
 
-# (1) What will it print now?
-p1 = Person("Scott")
-p2 = Person("Alex")
-print(id(p1))
-print(id(p2))
-p1.say_hello()
+room = [] # (2)
+room.append() # (3)
+
+
+# (1) Combining Person and list, imagine we had to write an application for managing a classroom
+# (2) So we'll have to handle multiple Person objects and group them into a specific classroom
+# (3) Now I can start appending Person objects into that list
