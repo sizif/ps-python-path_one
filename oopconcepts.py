68217a07bbcd426616863aad24b91daf6b7f9f02
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 14:28:33 +0200

    (1) Instead of hardcoding the name argument in __init__() with the value "Scott", we can parametrize it, which is of course much better

--- oopconcepts.py
+++ oopconcepts.py
@@ -1,8 +1,8 @@
 # CREATE OUR OWN OBJECT
 class Person:
 
-    def __init__(self):
-        self.name = "Scott"
+    def __init__(self, name):
+        self.name = name
 
     def say_hello(self):
         print("Hello, ", self.name)
