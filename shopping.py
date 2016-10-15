46eb312391a314810ffc627924bd303c90b946fb
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 12:05:51 +0200

    (1) And I can do simple string comparisons on 'command' (in def process_order), to figure out what the user wants to do; # (2) 'q' will return False and the function will exit at this point and return the false to the while loop in go_shopping(): (3) so down here I don't have to check the item anymore (# if item == ""), instead I'll replace it with and if statement to check the return value of process_order();

--- shopping.py
+++ shopping.py
@@ -11,15 +11,24 @@ def go_shopping():
     cart = []
     
 
-def process_order(order, cart): # (1) so process_order takes an order and a cart
-    command, item = order # (2) and I can unpack them here (this is an example of usefulness of tuple)
+def process_order(order, cart):
+    command, item = order
+
+    # (1) And I can do simple string comparisons on 'command' to figure out what the user wants to do:
+    if command == "a":
+        cart.append(item)
+    elif command == "d":
+        cart.remove(item)
+    elif command == "q": # (2) 'q' will return False and the function will exit at this point and return the false to the while loop in go_shopping(): (3)
+        return False
 
     while True: 
         order = get_order()
 
         process_order(order, cart)
-        if item == "": 
-            break
+        # (3) so down here I don't have to check the item anymore, instead I'll replace it with and if statement to check the return value of process_order()
+        # if item == "":
+        #    break
 
     print(cart)
     print("Finished!")
