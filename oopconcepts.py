fed8a1a189f5ad1b27bc997f1eaf1fc65224632c
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 15:01:57 +0200

    (1) What will it print now?

--- oopconcepts.py
+++ oopconcepts.py
@@ -8,12 +8,9 @@ class Person:
         print(id(self)) # (3)
         print("Hello, ", self.name)
 
-# (1) Will p1.say_hello() print out, Scott or Alex
+# (1) What will it print now?
 p1 = Person("Scott")
-p2 = p1
-p2.name = "Alex"
-p1.say_hello()
-
-# (2) we'll verify the result by priting the id's for both objects
+p2 = Person("Alex")
 print(id(p1))
 print(id(p2))
+p1.say_hello()
