ffb8e76411da8854b4349fd119b1fac94106345b
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 13:12:36 +0200

    Sets are powerful (look at the comments at the bottom of the file)

--- shopping.py
+++ shopping.py
@@ -11,7 +11,7 @@ def process_order(order, cart):
     command, item = order
 
     if command == "a":
-        cart.add(item) # (2)
+        cart.add(item)
     elif command == "d" and item in cart:
         cart.remove(item)
     elif command == "q":
@@ -20,7 +20,7 @@ def process_order(order, cart):
     return True
 
 def go_shopping():
-    cart = set() # (1)
+    cart = set()
 
     while True:
         order = get_order()
@@ -36,13 +36,15 @@ def go_shopping():
     
 go_shopping()
 
-# 3 data structures:
-alist = [ 1, 2, 3 ]
-atuple = ( 1, 2, 3 ) # a tuple is essentially a list that you cannot change, you cannot add to it or remove from it
-aset = set() # the built in set() function gives me an empty set
-asettwo = { 1, 2, 3 } # I can init my set with say 3 numbers, and curly braces
-# a set is unordered! aset.add(0) vs alist.append(0)
-# a set is trying to prevent duplicate entries!
-# all the values inside of a set have to be unique!
-# (1) So in my program, I'll change my cart from being a list to being a set
-# (2) And I'll also change append() method to add()
+# set is an UNORDERED collection of UNIQUE values
+# Example:
+# s1 = { 1, 2, 3 }
+# s2 = { 2, 3, 4 }
+# len(s1) returns '3'
+# s1[0] returns 'TypeError: 'set' object does not support indexing'
+# s1.difference(s2) returns '{1}', it can also be written like s1 - s2
+# s1 - s2 returns {1}; s2 - s1 returns {4}
+# Asking for numbes that are in both sets: s1 & s2; returns {2, 3}
+# What items are in s1 or s2 but not both: s1 ^ s2; returns {1, 4}
+
+
