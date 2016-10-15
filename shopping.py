67330845b0cb1514bc3fea4368f806b0f4fa07c4
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 13:27:28 +0200

    Dictionaries notes are on the bottom of the file

--- shopping.py
+++ shopping.py
@@ -36,15 +36,28 @@ def go_shopping():
     
 go_shopping()
 
-# set is an UNORDERED collection of UNIQUE values
-# Example:
-# s1 = { 1, 2, 3 }
-# s2 = { 2, 3, 4 }
-# len(s1) returns '3'
-# s1[0] returns 'TypeError: 'set' object does not support indexing'
-# s1.difference(s2) returns '{1}', it can also be written like s1 - s2
-# s1 - s2 returns {1}; s2 - s1 returns {4}
-# Asking for numbes that are in both sets: s1 & s2; returns {2, 3}
-# What items are in s1 or s2 but not both: s1 ^ s2; returns {1, 4}
+# DICTIONARIES:
+# To construct a dict, I can use a built-in function dict():
+# adict = dict() constructs a new empty dictionary
+# or I can create it just like a set, with {} braces, but while the set had single items,
+# a dictionary holds key-value pairs:
+# adict = { "scott":"@OdeToCode", "pluralsight":"@Pluralsight" }
+# but I cant offset into them like this: adict[0] as this returns a KeyError
+# Instead, I use the value from the key:value pair:
+# adict["scott"]
+# You can also iterate through a dictionary, and return keys:
+# for key in adict:
+#   print(key)
+# And you can return values too:
+# for key in adict:
+#   print(adict[key]) # "give me a value for that particular key"
+# Finally, I can print them both out:
+# for key in adict:
+#   print(key, adict[key])
+# "Dear dictionary, here's a new value to store:
+# adict["count"] = 4
+
+# THE KEYS DON'T HAVE TO BE STRINGS
+# They can be different types of objects.
 
 
