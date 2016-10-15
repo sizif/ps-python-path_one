78b57783537b026d36ac66683e5780b1c591e286
ajdika <ajdika@live.com>
2016-10-15 05:21:24 -0400

    cart.append(item); (1) I can no longer just cart.append(item), I have to figure out what's in the order; (2) So it might be a good idea to do some top-down design and just hand this off to another function: (3)
    (3) I'll call it process_order(), and I'll give it the order and the cart; (4) It's the job of process_order() to figure out what to add to the cart or remove from it

--- shopping.py
+++ shopping.py
@@ -5,16 +5,23 @@ def get_order():
     command = line[:1] 
     item = line[2:] 
 
-    return command, item # (1) return command and item in def get_order()
+    return command, item
     
 def go_shopping():
     cart = []
-  
+    
+# (4) It's the job of process_order() to figure out what to add to the cart or remove from it
+def process_order(order, cart):
+    
     while True: 
-        order = get_order() # (2) 'order' instead of 'item' for var name in def go_shopping(), because it's a command on an item instead of just some item
+        order = get_order()
+        # (3) I'll call it process_order(), and I'll give it the order and the cart
+        process_order(order, cart)
         if item == "": 
             break
-        cart.append(item)
+        # cart.append(item)
+        # (1) I can no longer just cart.append(item), I have to figure out what's in the order
+        # (2) So it might be a good idea to do some top-down design and just hand this off to another function: (3)
         
     print(cart)
     print("Finished!")
