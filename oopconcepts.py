b0f1c0727072f94a54729583f339d5cbacdf9a8f
ImsirovicAjdin <ImsirovicAjdin@users.noreply.github.com>
2016-10-15 14:26:53 +0200

    (1) __init__ constructor method example; (2) overriding default behavior in object instance (polymorphism?)

--- oopconcepts.py
+++ oopconcepts.py
@@ -1,8 +1,15 @@
 # CREATE OUR OWN OBJECT
 class Person:
 
+    def __init__(self):
+        self.name = "Scott"
+
     def say_hello(self):
-        print("Hello!")
+        print("Hello, ", self.name)
 
 p1 = Person()
+p1.name = "Allen"
 p1.say_hello()
+
+p2 = Person()
+p2.say_hello()
